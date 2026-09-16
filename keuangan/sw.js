ff3526bb7f6c9f10c47f1cdd0366530e6909bf3b
Why Guitar Pky <wahyudiacnion3@gmail.com>
2026-09-16 11:54:19 +0700

    Add files via upload

---
+++ keuangan/sw.js
@@ -0,0 +1,9 @@
+const CACHE='why-guitar-keuangan-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET') return;
+ e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{
+  const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+ }).catch(()=>cached)));
+});
